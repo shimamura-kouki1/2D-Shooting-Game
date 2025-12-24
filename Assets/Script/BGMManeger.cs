57c219bd43823d5396604488e5fa61fe6827985d
shimamura <shimakou779@gmail.com>
2025-12-24 11:06:57 +0900

    [add]シーンごとにBGMが再生されるようにした

---
+++ Assets/Script/BGMManeger.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class BGMManeger : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] _bgm;
+    private AudioSource _audioSource;
+    private int _unmber = 0;
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.resource = _bgm[_unmber];
+        _audioSource.Play();
+    }
+
+    private void Update()
+    {
+        //Titelかつ、要素が0以外なら実行
+        if (GameManeger.Instance.CurrentState == GameState.Titel && _bgm[_unmber] != _bgm[0])
+        {
+            _unmber = 0;
+            _audioSource.resource = _bgm[0];
+            _audioSource.Play();
+        }
+        if(GameManeger.Instance.CurrentState == GameState.Playing && _bgm[_unmber] != _bgm[1])
+        {
+            _unmber = 1;
+            _audioSource.resource= _bgm[1];
+            _audioSource.Play();
+        }
+        if(GameManeger.Instance.CurrentState == GameState.GameOver && _bgm[_unmber] != _bgm[2])
+        {
+            _unmber = 2;
+            _audioSource.resource = _bgm[2]; //(AudioClip)_bgm[2];
+            _audioSource.Play();
+        }
+
+    }
+}
