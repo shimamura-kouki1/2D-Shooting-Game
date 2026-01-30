c0bbcdaecae8d8ae36f522ad9042167373882eb9
shimamura <shimakou779@gmail.com>
2026-01-30 21:57:34 +0900

    コミット忘れ　特に変えてない

--- Assets/Script/Enemy/EnemyBullet.cs
+++ Assets/Script/Enemy/EnemyBullet.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class EnemyBullet : MonoBehaviour
 {
     public EnemyBulletPool _enemybulletPool { get; set; }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         
